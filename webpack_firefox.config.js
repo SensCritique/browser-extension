69a0003e8bf0e800d4e2db2eac41778b2a6818d1
bt0r <the.btor@gmail.com>
2020-08-01 10:47:00 +0200

    fix: Observe appMountPoint DOM node than .mainView because mainView is created dynamically

--- webpack_firefox.config.js
+++ webpack_firefox.config.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: {
-    index:  './src/index.js',
-    background:  './src/background.js',
+    index: './src/index.js',
+    background: './src/background.js'
   },
   plugins: [
     new CopyPlugin([
       { from: 'images/logo*', to: '.' },
-      { from: 'manifest_firefox.json', to: 'manifest.json'}
-    ]),
+      { from: 'manifest_firefox.json', to: 'manifest.json' }
+    ])
   ],
   output: {
     path: path.resolve(__dirname, 'dist/firefox/main'),
-    filename: '[name]/index.js',
+    filename: '[name]/index.js'
   }
-};
+}

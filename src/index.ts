7e46e79f0b269d984da953e67b467d775eb87d2a
btor <the.btor@gmail.com>
2020-11-19 23:41:11 +0100

    fix: use Timer + ratingsElement presence to show modals

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import Manager from './dom/Manager'
 import { Netflix } from './config/Netflix'
+import md5 from 'blueimp-md5'
 
 const manager = new Manager()
-let jawboneEventFound = false
+let timer = 0
 
 setInterval(() => {
   manager.refreshRatings()
@@ -10,10 +11,15 @@ setInterval(() => {
 
 // Check if user has accepted AB Tests
 setInterval(async () => {
-  if (!jawboneEventFound && manager.currentVideoId() !== null && await Netflix.canABTest()) {
+  const videoName = manager.getVideoName()
+  const ratingsHash = md5(videoName)
+  const ratingsElements = document.getElementsByClassName(ratingsHash)
+
+  if (ratingsElements.length === 0 && timer > 3 && manager.currentVideoId() !== null && await Netflix.canABTest()) {
     manager.showAbTestModal()
   }
-  if (!jawboneEventFound && manager.currentVideoId() !== null && !await Netflix.canABTest()) {
+  if (ratingsElements.length === 0 && timer > 3 && manager.currentVideoId() !== null && !await Netflix.canABTest()) {
     manager.showNotSupportedModal()
   }
+  timer = timer + 1
 }, 5000)

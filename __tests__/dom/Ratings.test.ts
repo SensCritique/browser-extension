d81246b31685c834577bb7824084710844a17541
audreylamy <audrey.lamy92@gmail.com>
2022-09-23 17:06:48 +0200

    fix test

--- __tests__/dom/Ratings.test.ts
+++ __tests__/dom/Ratings.test.ts
@@ -5,6 +5,6 @@ test('It should render two div, one for SensCritique, one for Allociné', () => 
   const ratingsDiv = Ratings.render('XXX')
 
   expect(ratingsDiv).not.toBeNull()
-  expect(ratingsDiv.childNodes.length).toBe(2)
+  expect(ratingsDiv.childNodes.length).toBe(1)
   expect((ratingsDiv.childNodes[1] as Element).classList).toContain('senscritique_XXX')
 })

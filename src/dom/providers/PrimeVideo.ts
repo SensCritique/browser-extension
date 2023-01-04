43425bdab7b8ba69ccbf200bda7c1c6ab7b61a03
audreylamy <audrey.lamy92@gmail.com>
2023-01-04 11:42:31 +0100

    add rating primevideo

---
+++ src/dom/providers/PrimeVideo.ts
@@ -0,0 +1,124 @@
+import Cache from '../../storage/Cache'
+import Ratings from '../Ratings'
+import md5 from 'blueimp-md5'
+import { Service } from '../../http/Service'
+import { Provider } from '../../http/Provider'
+import Logger from '../../logging/Logger'
+import Manager from '../Manager'
+import { VideoType } from '../../http/VideoType'
+import { VideoInfo } from '../../http/Client'
+import RatingFactory from '../RatingFactory'
+
+const manager = new Manager()
+
+export default class PrimeVideo {
+  private cache: Cache
+  private logger: Logger
+
+  constructor () {
+    this.cache = new Cache()
+    this.logger = new Logger()
+  }
+
+  refreshRatings () {
+    const videoName = this.getVideoName()
+    const modal = document.querySelector('.dv-node-dp-badges')
+    const hash = md5(videoName)
+
+    if (videoName && modal?.getElementsByClassName(hash).length === 0) {
+      if (modal) {
+        const ratingsElement = Ratings.render(hash, Provider.PRIME)
+        modal.prepend(ratingsElement)
+      }
+      this.getRating(videoName, modal, Service.SENSCRITIQUE, hash)
+    }
+  }
+
+  getVideoName (): string | null {
+    let detailModalVideoName = null
+    detailModalVideoName = document
+      .querySelector('._2IIDsE')
+      .textContent
+
+    if (!detailModalVideoName) {
+      detailModalVideoName = document
+        .querySelector('._1GS1_C')
+        ?.getAttribute('alt')
+    }
+
+    return detailModalVideoName || null
+  }
+
+  getVideoYear (): string {
+    const element: any = document.querySelectorAll('.XqYSS8')[1]?.firstElementChild
+    console.log(element?.innerText)
+    return element?.innerText
+  }
+
+  getVideoType (): VideoType {
+    const element = document.querySelector('._1J8qi6')
+    const textContent = element?.textContent.toLowerCase()
+    return textContent?.includes('saison') ? VideoType.TVSHOW : VideoType.MOVIE
+  }
+
+  getRating (videoName: string, jawbone: Element, service: Service, hash: string): void {
+    // const videoInfoFound = this.cache.get(videoName, service)
+    const videoInfoFound = null
+    if (!videoInfoFound) {
+      manager.getVideoInfo(
+        service,
+        videoName,
+        this.getVideoYear(),
+        this.getVideoType(),
+        (videoInfo: VideoInfo) => {
+          this.renderRating(service, jawbone, videoInfo, hash)
+        }
+      )
+    }
+    if (videoInfoFound) {
+      this.renderRating(service, jawbone, videoInfoFound, hash)
+    }
+  }
+
+  renderRating (service: Service, element: Element, videoInfo: VideoInfo, hash: string): void {
+    this.cache.save(videoInfo, service)
+
+    const serviceRating = new RatingFactory().create(service, videoInfo)
+    const ratingElement = serviceRating.render()
+
+    document
+      .querySelectorAll(`.${service}_${hash}`)
+      .forEach((serviceElement) => {
+        if (serviceElement.childNodes.length === 0) {
+          serviceElement.innerHTML = ratingElement.outerHTML
+          this.logVideoInfo(videoInfo.name, serviceRating.rating, service)
+        }
+      })
+  }
+
+  logVideoInfo (videoName: string, rating: string, service: Service): void {
+    if (rating) {
+      this.logger.info(`Rating fetched for video ${videoName}`, {
+        name: videoName,
+        rating: rating,
+        serviceWebsite: service,
+        prime_video_id: this.currentVideoId(),
+        provider: Provider.PRIME
+      })
+    } else {
+      this.logger.error(`Cannot fetch rating for video ${videoName}`, {
+        name: videoName,
+        serviceWebsite: service,
+        prime_video_id: this.currentVideoId(),
+        provider: Provider.PRIME
+      })
+    }
+  }
+
+  currentVideoId (): string {
+    const urlQuery = new URL(window.location.toString()).pathname.split(
+      '/'
+    )
+    return urlQuery.length > 0 && urlQuery[2] ? urlQuery[2] : null
+  }
+}
--- src/enum/ProviderUrlDomain.ts
+++ src/enum/ProviderUrlDomain.ts
@@ -1,4 +1,5 @@
 export enum ProviderUrlDomain {
   NETFLIX = 'www.netflix.com',
   DISNEY = 'www.disneyplus.com',
+  PRIME = 'www.primevideo.com',
 }

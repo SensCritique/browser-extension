e5a7e3735fdafd16a724768b9448cc9664709371
audreylamy <audrey.lamy92@gmail.com>
2023-01-11 10:28:46 +0100

    find by url domain

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,42 @@ import Netflix from './dom/providers/Netflix'
 import Disney from './dom/providers/Disney'
 import { NetflixConfig } from './config/Netflix'
 import { Provider } from './http/Provider'
+import { ProviderUrlDomain } from './http/ProviderUrlDomain'
 import md5 from 'blueimp-md5'
 
 const netflix = new Netflix()
 const disney = new Disney()
+const domainOrigin = (new URL(window.location.href))?.origin
 let timer = 0
 
 setInterval(() => {
-  if (window.location.href && window.location.href.includes(Provider.DISNEY)) disney.refreshRatings()
-  else netflix.refreshRatings()
+  console.log('domain', domainOrigin)
+  if (domainOrigin) {
+    switch (domainOrigin) {
+      case ProviderUrlDomain.DISNEY:
+        disney.refreshRatings()
+        break
+      default:
+        netflix.refreshRatings()
+        break
+    }
+  }
 }, 1000)
 
 // Check if user has accepted AB Tests
 setInterval(async () => {
-  let videoName = null
-  if (window.location.href && window.location.href.includes(Provider.DISNEY)) videoName = disney.refreshRatings()
-  else videoName = netflix.refreshRatings()
-  const ratingsHash = md5(videoName)
-  const ratingsElements = document.getElementsByClassName(ratingsHash)
-
   /*
    * Temporary disable modals
+  switch (domainOrigin) {
+    case ProviderUrlDomain.DISNEY:
+      disney.refreshRatings()
+      break
+    default:
+      netflix.refreshRatings()
+      break
+  }
+  const ratingsHash = md5(videoName)
+  const ratingsElements = document.getElementsByClassName(ratingsHash)
 
   if (ratingsElements.length === 0 && timer > 3 && manager.currentVideoId() !== null && await Netflix.canABTest()) {
     manager.showAbTestModal()

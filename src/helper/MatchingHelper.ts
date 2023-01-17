b013c2c305efd0a2b0165bcb4a581ebed250217b
audreylamy <audrey.lamy92@gmail.com>
2023-01-17 17:02:59 +0100

    fix wording method

--- src/helper/MatchingHelper.ts
+++ src/helper/MatchingHelper.ts
@@ -1,11 +1,12 @@
-export const numberMatched = (number1: number, number2: number): boolean => {
+export const numbersMatched = (number1: number, number2: number): boolean => {
   if (number1 && number2) {
+    console.log(number1 === number2)
     return number1 === number2
   }
   return false
 }
 
-export const stringMatched = (string1: string, string2: string): boolean => {
+export const stringsMatched = (string1: string, string2: string): boolean => {
   if (string1 && string2) {
     return string1 === string2
   }

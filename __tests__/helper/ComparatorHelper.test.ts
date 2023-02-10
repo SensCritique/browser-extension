25faae6102d83862ddd59ef3acdbd3a44112be8b
audreylamy <audrey.lamy92@gmail.com>
2023-02-10 11:42:05 +0100

    fix(test): add tests comparator + datasets

--- __tests__/helper/ComparatorHelper.test.ts
+++ __tests__/helper/ComparatorHelper.test.ts
@@ -1,7 +1,15 @@
-import { VideoType } from './../../src/enum/VideoType'
 import { test, expect } from '@jest/globals'
 import { compare } from '../../src/helper/ComparatorHelper'
 import { Logger } from '../../src/background'
+import {
+  matchByTitleAndYearDataset,
+  matchByMovieAndYearDataset,
+  matchYearAndSeasonIfTvShowDataset,
+  matchByTitleAndSeasonIfTvShowDataset,
+  notMatchByTypeOrProviderDataset,
+  notMatchByTitleAndYearIfMovieDataset,
+  notMatchByTitleYearAndSeasonsIfTvShowDataset
+} from '../../datasets/CamparatorHelper'
 
 jest.mock('../../src/background', () => {
   return {
@@ -17,38 +25,41 @@ jest.mock('../../src/background', () => {
   }
 })
 
-const matchByTitleDataset = [
-  [{
-    flattenedOriginalTitle: null,
-    flattenedTitle: 'brooklyn ninenine',
-    nbrSeasons: 8,
-    originalTitle: null,
-    rating: 7.5,
-    title: 'Brooklyn Nine-Nine',
-    type: VideoType.TVSHOW,
-    url: '/serie/brooklyn_nine_nine/8855898',
-    year: 2021,
-    providers: ['Disney+']
-  }, {
-    flattenedTitle: 'brooklyn ninenine',
-    nbrSeasons: 8,
-    title: 'Brooklyn Nine-Nine',
-    type: VideoType.TVSHOW,
-    year: 2021,
-    providers: ['disney']
-  }, {
-    name: 'Brooklyn Nine-Nine',
-    redirect: 'https://www.senscritique.com/serie/brooklyn_nine_nine/8855898',
-    id: VideoType.TVSHOW,
-    url: 'https://www.senscritique.com/serie/brooklyn_nine_nine/8855898',
-    type: VideoType.TVSHOW,
-    rating: '7.5'
-  }]
-]
-
 describe('Test products matching with compare()', () => {
-  test.each(matchByTitleDataset)('It should return video infos if title and year match', async (senscritiqueProduct, platformProduct, expectedVideoInfo) => {
+  test.each(matchByTitleAndYearDataset)('It should return video infos if title and year match', async (senscritiqueProduct, platformProduct, expectedVideoInfo) => {
     const videoInfo = await compare(senscritiqueProduct, platformProduct)
     expect(videoInfo).toEqual(expectedVideoInfo)
+  })
+
+  test.each(matchByMovieAndYearDataset)('It should return video infos if the product is a movie and year match', async (senscritiqueProduct, platformProduct, expectedVideoInfo) => {
+    const videoInfo = await compare(senscritiqueProduct, platformProduct)
+    expect(videoInfo).toEqual(expectedVideoInfo)
+  })
+
+  test.each(matchYearAndSeasonIfTvShowDataset)('It should return video infos if the product is a tvShow and year, season match', async (senscritiqueProduct, platformProduct, expectedVideoInfo) => {
+    const videoInfo = await compare(senscritiqueProduct, platformProduct)
+    expect(videoInfo).toEqual(expectedVideoInfo)
+  })
+
+  test.each(matchByTitleAndSeasonIfTvShowDataset)('It should return video infos if the product is a tvShow and title, season match', async (senscritiqueProduct, platformProduct, expectedVideoInfo) => {
+    const videoInfo = await compare(senscritiqueProduct, platformProduct)
+    expect(videoInfo).toEqual(expectedVideoInfo)
+  })
+})
+
+describe('Test products does not match with compare()', () => {
+  test.each(notMatchByTypeOrProviderDataset)('It should return null if type or provider not match', async (senscritiqueProduct, platformProduct) => {
+    const videoInfo = await compare(senscritiqueProduct, platformProduct)
+    expect(videoInfo).toBeNull()
+  })
+
+  test.each(notMatchByTitleAndYearIfMovieDataset)('It should return null if the product is a movie but the title and year not match', async (senscritiqueProduct, platformProduct) => {
+    const videoInfo = await compare(senscritiqueProduct, platformProduct)
+    expect(videoInfo).toBeNull()
+  })
+
+  test.each(notMatchByTitleYearAndSeasonsIfTvShowDataset)('It should return null if the product is a tvShow but title, year and season not match', async (senscritiqueProduct, platformProduct) => {
+    const videoInfo = await compare(senscritiqueProduct, platformProduct)
+    expect(videoInfo).toBeNull()
   })
 })
